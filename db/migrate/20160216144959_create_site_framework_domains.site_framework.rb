5285caf898c67a6a1a7d2edbf8733f5d0b021a55
Sameer Rahmani <lxsameer@gnu.org>
2016-02-16 18:23:06 +0330

    site_framework deployed

---
+++ db/migrate/20160216144959_create_site_framework_domains.site_framework.rb
@@ -0,0 +1,15 @@
+# This migration comes from site_framework (originally 20140118200201)
+class CreateSiteFrameworkDomains < ActiveRecord::Migration
+  def change
+    create_table :site_framework_domains do |t|
+      t.string :name
+      t.integer :site_id
+      t.integer :parent_id, default: nil
+      t.boolean :alias, default: false
+
+      t.timestamps
+    end
+
+    add_index :site_framework_domains, :name, unique: true
+  end
+end
