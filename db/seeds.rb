cb33189bf04e4ed3a52fdb40018b5cf16d885bc1
Behnam Ahmad Khan Beigi <yottanami@gnu.org>
2016-02-16 19:33:51 +0330

    Seeds created

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 Faalis::Engine.load_seed
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
@@ -6,3 +7,19 @@ Faalis::Engine.load_seed
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+radio_boot = SiteFramework::Site.create!(title: 'رادیو بوت',
+  description: 'پادکست رادیو بوت، به بهانه تکنولوژی',
+  default_template: 'radio_boot')
+
+SiteFramework::Domain.create!(site: radio_boot,
+  name:'radioboot.com', alias: 'www.radioboot.com')
+
+
+tech5 = SiteFramework::Site.create!(title: 'Tech5',
+  description: 'پادکست تک فایو کار مشترکی از سلام دنیا و رادیو بوت',
+  default_template: 'radio_boot')
+
+SiteFramework::Domain.create!(site: tech5,
+  name:'tech5.ir',
+  alias: 'www.tech5.ir')
