16a6f406c1f0541c0cd12380d5ab5ea5192bee5b
Sameer Rahmani <lxsameer@gnu.org>
2016-02-16 17:15:22 +0330

    renamed to community master

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_RadioMaster_session'
+Rails.application.config.session_store :cookie_store, key: '_CommunityMaster_session'
