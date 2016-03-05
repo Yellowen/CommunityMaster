1664010e0d2f7e2285861eb683227bd40dccef2e
Sameer Rahmani <lxsameer@gnu.org>
2016-03-05 04:48:51 -0800

    useless code removed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,7 @@
 class ApplicationController < ActionController::Base
   extend Faalis::I18n::Locale
 
-  before_action :load_external_templates_path
-
-
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-
-  private
-
-  def load_external_templates_path
-    #prepend_view_path CommunityMaster.external_view_path
-    puts "HELLO" * 100, view_paths
-  end
 end
