854dcdd9020af1b84441a22b5501cd4e63365a95
Behnam Ahmad Khan Beigi <yottanami@gnu.org>
2016-03-15 18:01:08 +0330

    demo domain created

--- db/seeds.rb
+++ db/seeds.rb
@@ -30,4 +30,4 @@ ciwic = SiteFramework::Site.create!(title: 'شرکت بین المللی کاس�
   default_template: 'canvas')
 
 SiteFramework::Domain.create!(site: ciwic,
-  name: 'ciwic.local')
+  name: 'ciwic.local', alias: 'ciwic.faalis.io')
