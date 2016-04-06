c3fdaec9b45cdeedde947adc75d6645b02602ebe
Sameer Rahmani <lxsameer@gnu.org>
2016-04-06 14:03:09 +0430

    test_helper improved to use fabrication, capybara and other useful tools

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,10 +1,72 @@
-ENV['RAILS_ENV'] ||= 'test'
-require File.expand_path('../../config/environment', __FILE__)
-require 'rails/test_help'
+ENV["RAILS_ENV"] ||= 'test'
+
+require File.expand_path("../../config/environment", __FILE__)
+require "rails/test_help"
+#require "mocha/mini_test"
+
+# FABRICATION -----------------------------------
+require 'fabrication'
+
+Fabrication.configure do |config|
+  config.fabricator_path = 'test/fabricators/'
+  config.path_prefix = Faalis::Engine.root
+end
+
+# MINITEST REPORTER -----------------------------
+require "minitest/reporters"
+
+Minitest::Reporters.use!(Minitest::Reporters::ProgressReporter.new,
+                         ENV,
+                         Minitest.backtrace_filter)
+
 
 class ActiveSupport::TestCase
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
-
-  # Add more helper methods to be used by all tests here...
 end
+
+# CAPYBARA ---------------------------------------
+# Capybara and poltergeist integration
+require 'minitest/rails/capybara'
+require 'capybara/rails'
+require 'capybara/poltergeist'
+
+class ActionDispatch::IntegrationTest
+  include Capybara::DSL
+  include Capybara::Assertions
+  include Faalis::Engine.routes.url_helpers
+  include Warden::Test::Helpers
+
+  Warden.test_mode!
+
+  after do
+    Warden.test_reset!
+  end
+end
+
+Capybara.javascript_driver = :poltergeist
+
+
+# DATABASE CLEANER -------------------------------
+
+require 'database_cleaner'
+require 'minitest/around/spec'
+
+class Minitest::Spec
+  around do |tests|
+    DatabaseCleaner.cleaning(&tests)
+  end
+end
+
+# CONNECTION POOL --------------------------------
+require 'connection_pool'
+
+class ActiveRecord::Base
+  mattr_accessor :shared_connection
+  @@shared_connection = nil
+
+  def self.connection
+    @@shared_connection || ::ConnectionPool::Wrapper.new(:size => 1) { retrieve_connection }
+  end
+end
+
+ActiveRecord::Base.shared_connection = ActiveRecord::Base.connection
