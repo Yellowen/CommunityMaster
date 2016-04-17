fd189b1087aa3ff5c8905ec46e34c3f1283992a8
Sameer Rahmani <lxsameer@gnu.org>
2016-04-17 12:08:21 +0430

    Single bug fixed in test_helpers

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -9,7 +9,7 @@ require 'fabrication'
 
 Fabrication.configure do |config|
   config.fabricator_path = 'test/fabricators/'
-  config.path_prefix = Faalis::Engine.root
+  config.path_prefix = Rails.root
 end
 
 # MINITEST REPORTER -----------------------------
