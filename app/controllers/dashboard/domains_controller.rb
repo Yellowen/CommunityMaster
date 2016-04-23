91d0cafd1bf368cba886ee76eb1549b4d4780325
Sameer Rahmani <lxsameer@gnu.org>
2016-04-23 12:49:57 -0700

    domain dashboard controller bug fixed

--- app/controllers/dashboard/domains_controller.rb
+++ app/controllers/dashboard/domains_controller.rb
@@ -1,24 +1,15 @@
 class Dashboard::DomainsController < Dashboard::ApplicationController
   #engine 'SiteFramework::Engine'
-  model_name '::SiteFramework::Domain'
+  model_name 'SiteFramework::Domain'
 
   in_form do |form|
-    form.attributes except: [:updated_at, :user]
-  end
-
-  protected
-
-  def model_name
-    'SiteFramework::Domain'
+    form.attributes except: [:user]
   end
 
   private
 
   def before_create_hook(resource)
-    if !current_user.admin?
-      resource.user = current_user
-      resource.candidate = SiteFramework::Domain.find_by(user_id: current_user.id)
-    end
+    resource.user = current_user
   end
 
 end
