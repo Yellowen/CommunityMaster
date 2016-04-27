abf934c51eec3ef496f772573c02422259883217
Behnam Ahmad Khan Beigi <yottanami@gnu.org>
2016-04-27 16:47:32 +0430

    site bug fixed

--- db/migrate/20160423091604_patch_domain.rb
+++ db/migrate/20160423091604_patch_domain.rb
@@ -1,9 +1,11 @@
 class PatchDomain < ActiveRecord::Migration[5.0]
   def up
     add_column :site_framework_domains, :namespace_id, :integer
+    add_column :site_framework_domains, :user_id, :integer
   end
 
   def down
     remove_column :site_framework_domains, :namespace_id
+    remove_column :site_framework_domains, :user_id
   end
 end
