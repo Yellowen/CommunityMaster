2cadd54a09dccd6f232636b8fafa16292b6d2b4e
Behnam Ahmad Khan Beigi <yottanami@gnu.org>
2016-05-07 19:16:12 +0430

    before_create_hook modified in site dashboard"

--- app/controllers/dashboard/sites_controller.rb
+++ app/controllers/dashboard/sites_controller.rb
@@ -11,8 +11,37 @@ class Dashboard::SitesController < Dashboard::ApplicationController
   private
 
   def before_create_hook(resource)
-    resource.user = current_user
+    domain = 'faalis.io'
+    resource.user_id = current_user.id
 
+    if (!params.has_key?(:name) || !params.has_key?(:namespace))
+      flash[:error] = t('Fill in the blank')
+      byebug
+    else
+
+      domain = SiteFramework::Domain.where(
+        name: params[:name] + '-' + domain)
+
+      if domain.exists?
+        flash[:error] = t('Domain exists')
+        byebug
+      end
+
+    end
+  end
+
+  def after_create_hook(resource)
+    byebug
+    namespace = Namespace.find_or_create_by(
+      name: params[:namespace],
+      user: current_user)
+
+    SiteFramework::Domain.create(
+      namespace: namespace,
+      name: params[:name] + '-' + domain,
+      user: current_user,
+      site_id: resource.id
+    )
   end
 
   def new_hook(resource)
