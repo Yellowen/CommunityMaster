737235f36543067939bf324458549662d306337c
Sameer Rahmani <lxsameer@gnu.org>
2016-05-23 18:13:27 +0430

    manifest.js added

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -2,6 +2,6 @@
 //
 //= link_directory ../javascripts .js
 //= link_directory ../stylesheets .css
+//
 //= link_tree ../images
-//= link faalis/simple/ltr/application.css
-//= link dashboard/application.js
+//= link_tree ../fonts
