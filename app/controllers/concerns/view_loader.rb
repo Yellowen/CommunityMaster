39d6ca33940aa797cb3601d9b0b9f02bc25f55dc
Sameer Rahmani <lxsameer@gnu.org>
2016-05-29 16:45:31 +0430

    view loader problem fixed

--- app/controllers/concerns/view_loader.rb
+++ app/controllers/concerns/view_loader.rb
@@ -6,58 +6,41 @@ module Concerns
 
     PER_SITE_VIEWS_PATH = Rails.application.sites_tree
 
-    included do
-      # Override the view path. check the class method
-      override_view_paths
-    end
-
-    module ClassMethods
-      # Simple remove the default view path (the one which loads the view from
-      # app/views). Sites should not have access to Mother App's views
-      def override_view_paths
-        #tmp = view_paths.to_a
-        tmp = view_paths.select do |p|
-          !p.to_s.start_with? Rails.root.to_s
-        end
-
-        self.view_paths = tmp
-      end
-    end
-
     private
 
     # This is a method to override the super method which is defined in
     # site framework. We altered the path creation process.
     def set_template_path
-      if request.respond_to?(:site) && request.site
+      #if request.respond_to?(:site) && SiteFramework.current_site
+      if SiteFramework.current_site
         # We are in site namespace
-        unless request.site.default_template.blank?
+        unless SiteFramework.current_site.default_template.blank?
           inject_views_paths(SiteFramework::Engine.view_path_prefix)
-          #inject_assets_paths(request.site.default_template)
+          inject_assets_paths(SiteFramework.current_site.default_template)
+          remove_default_paths
         end
-      else
-        prepend_view_path "#{Rails.root}/app/views"
+        #else
+        #prepend_view_path "#{Rails.root}/app/views"
       end
-
-      puts "====", view_paths.to_a
 
     end
 
-    def view_paths
-      @view_paths = super.select do |p|
-        puts "-", p.to_s.start_with?(Rails.root.to_s)
+    def remove_default_paths
+      return self._view_paths = @site_view_paths if @site_view_paths
 
-        if request.respond_to?(:site) && request.site
-          !p.to_s.start_with? Rails.root.to_s
-        end
+      tmp = view_paths.select do |p|
+        !p.to_s.start_with? Rails.root.to_s
       end
+
+      self._view_paths = ActionView::PathSet.new(tmp)
+      @site_view_paths = self.view_paths
 
     end
 
     def inject_views_paths(prefix)
-      prepend_view_path "#{prefix}/#{request.site.default_template}/views"
+      prepend_view_path "#{prefix}/#{SiteFramework.current_site.default_template}/views"
 
-      domain = request.site.identifier_domain.to_fqdn
+      domain = SiteFramework.current_site.identifier_domain.to_fqdn
       prepend_view_path "#{PER_SITE_VIEWS_PATH}/#{domain}/views"
     end
 
@@ -66,7 +49,7 @@ module Concerns
 
       if Rails.env.development? || Rails.env.test?
         theme_path = "#{Rails.root}/../template_master/templates/"
-        override_path = "#{PER_SITE_VIEWS_PATH}/#{request.site.id}/assets/"
+        override_path = "#{PER_SITE_VIEWS_PATH}/#{SiteFramework.current_site.id}/assets/"
       else
         theme_path = "#{Rails.root}/../../shared/template_master/templates/"
         override_path = nil
